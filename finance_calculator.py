f6b6e56fbe845be7193d4a0fe540e72700bb81fa
FATI785 <88990858+FATI785@users.noreply.github.com>
2025-03-09 22:11:39 +0100

    Anst python classwork and assessment
    ANST python classwork and assessment

---
+++ finance_calculator.py
@@ -0,0 +1,9 @@
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
+
+monthly_savings = monthly_income - monthly_expenses
+
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print(f"Your monthly savings are: {monthly_savings:.2f}")
+print(f"Your projected annual savings after interest are: {annual_savings:.2f}")
---
+++ simple_interest.py
@@ -0,0 +1,8 @@
+principal = 1000    #principal representing $1000
+rate = 0.05         #representing 5% annual interest rate
+time = 3            #representing 3 years
+
+# Calculate simple interest
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
